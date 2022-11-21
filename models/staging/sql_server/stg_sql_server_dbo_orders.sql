0d8155d16234c6a0e11b5aa814a615e3c50f212f
cristian.go.95@gmail.com <cristian.go.95@gmail.com>
2022-11-21 18:51:54 +0000

    2

--- models/staging/sql_server/stg_sql_server_dbo_orders.sql
+++ models/staging/sql_server/stg_sql_server_dbo_orders.sql
@@ -6,21 +6,21 @@ WITH src_orders AS (
 renamed_casted AS (
     SELECT
          ORDER_ID
-        ,SHIPPING_SERVICE
+        ,SHIPPING_SERVICE AS SERVICIO_ENVIO
         ,PROMO_ID
         ,ORDER_COST AS ORDER_COST_$
         ,TRACKING_ID
-        ,ESTIMATED_DELIVERY_AT
+        ,ESTIMATED_DELIVERY_AT 
         ,USER_ID
         ,SHIPPING_COST AS SHIPPING_COST_$
-        ,CREATED_AT TIMESTAMP_NTZ
+        ,CREATED_AT 
         ,ORDER_TOTAL AS ORDER_TOTAL_$
         ,STATUS
         ,ADDRESS_ID
         ,_fivetran_deleted
         ,_fivetran_synced
     FROM src_orders
-    WHERE ESTIMATED_DELIVERY_AT is not null
+    WHERE ESTIMATED_DELIVERY_AT is not null 
     )
 
 SELECT * FROM renamed_casted
